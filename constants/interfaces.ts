efb4ada1537627dcd9df72878b8b8bd52e48976c
PIYatGitHub <petyr.yonkov@mail.bg>
2021-01-24 23:12:20 +0200

    deps and consts

--- constants/interfaces.ts
+++ constants/interfaces.ts
@@ -19,10 +19,17 @@ export interface ITag {
 }
 
 
+export interface IHabitShedule{
+    day:number;
+    fromHour:string;
+    toHour:string; 
+}
 export interface IHabit {
     title: string;
-    goals: string[]
-    habitId: number
+    goals: string[];
+    habitId: number;
+    habitScheduleType: ScheduleTypes;
+    habitSchedule:IHabitShedule[]; 
 }
 
 export enum StatesEnum {
@@ -30,5 +37,32 @@ export enum StatesEnum {
     setGoals =  'setGoals',
     setScheduleType ='setScheduleType',
     showExamples='showExamples',
-    setHours = 'setHours'
+    setHours = 'setHours',
+    habitCreated = 'habitCreated'
+}
+
+export enum ScheduleTypes {
+    fixed = 'fixed',
+    fluid =  'fluid',
+    unknown =  'unknown',
+}
+
+export const weekDayMap = {
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday':3,
+    'Friday':4, 
+    'Saturday':5,
+    'Sunday':6
+}
+
+export enum DaysOfWeek {
+    'Monday'= 0,
+    'Tuesday'=  1,
+    'Wednesday' = 2,
+    'Thursday'= 3,
+    'Friday' =  4, 
+    'Saturday' = 5,
+    'Sunday' = 6
 }
