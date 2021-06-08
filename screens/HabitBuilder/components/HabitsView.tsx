5a6a2c719144f788834febc371d372b5898d8556
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2021-06-09 00:08:16 +0300

    Habits View Styles extracted

--- screens/HabitBuilder/components/HabitsView.tsx
+++ screens/HabitBuilder/components/HabitsView.tsx
@@ -4,7 +4,7 @@ import { IHabit, ScheduleTypes } from '../../../constants/interfaces';
 import React, { useState } from 'react';
 
 import colours from '../../../constants/Colours';
-import { commonStyles } from '../../styles/commonStyles';
+import { habitsViewStyles } from '../../styles/habitsViewStyles';
 import { convertNumberToWeekday } from '../../../utils/convertWeekday';
 
 interface iHabitProps {
@@ -77,35 +77,5 @@ const HabitsView = (props:iHabitProps) => {
 }
 export default HabitsView;
 
-const styles = StyleSheet.create({
-    blankHabitsContainer:{
-        marginLeft:windowWidth*0.1,
-        marginRight:windowWidth*0.1,
-        marginTop:windowHeight*0.2
-    },
-    blankHabitsContainerText:{
-        color: colours.ltGray,
-        fontSize:20,
-        textAlign:'center'
-    },
-    container: {
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'flex-start',
-        marginLeft:windowWidth*0.05,
-        marginRight:windowWidth*0.05,
-        marginTop:5,
-        borderWidth: 2,
-        borderColor:colours.ltGray,
-        borderRadius:10,
-        height: windowHeight*0.3,
-        maxHeight: windowHeight*0.3     
-    },
-    flexRowWrap: {
-        display:'flex',
-        flexDirection:'row', 
-        flexWrap:'wrap'
-    },
-    ...commonStyles
-  });
+const styles = habitsViewStyles;
   
