d703d420e6139744e50cbf13fe9ac55894936def
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2021-06-09 00:44:51 +0300

    top bar implemented

--- screens/HabitBuilder/components/TopBar.tsx
+++ screens/HabitBuilder/components/TopBar.tsx
@@ -7,28 +7,45 @@ import colours from '../../../constants/Colours';
 import { topBarStyles } from '../../styles/topBarStyles';
 import { convertNumberToWeekday } from '../../../utils/convertWeekday';
 
-interface iHabitProps {
-    habits: IHabit[]; 
-    filterByDay?:boolean;
-    filterByWeek?:boolean; 
-    onHabitDetails?: (habit:IHabit)=>void
+interface ITopBarProps {
+    tab: string;
+    show: boolean;
 }
 
-const TopBar = () => {
+const TopBar = (props:ITopBarProps) => {
     
     const handleTriggerHabitCreate = ()=>{
-        console.log(`Create Habit Pressed`)
+        console.log(`Menu Button Pressed`)
     }
     
-    return (        
-        <Container style={styles.actionBandMultipleAction}>
-            <Text style={styles.placeholder} uppercase={false}>+</Text>
-            <Text style={styles.centerText}>My habits</Text>
-            <Button style={styles.rightActionBtn} transparent onPress = {handleTriggerHabitCreate}>
-                <Text uppercase={false} style={styles.btnAddHabit}>+</Text>
-            </Button> 
-        </Container>
-    )
+    const handleCloseDetails = ()=>{
+        console.log(`Menu Button Pressed`)
+    }
+
+    if (props.tab !== 'settings' && !props.show){
+        return (
+            <Container style={styles.actionBandMultipleAction}>
+                <Button style={styles.rightActionBtn} transparent onPress = {handleTriggerHabitCreate}>
+                    <Icon style={styles.btnMenu} type='FontAwesome5' name='ellipsis-v'/>
+                </Button> 
+                <Text style={styles.centerText}>My habits</Text>
+                <Text style={styles.placeholder} uppercase={false}>+</Text>
+            </Container>
+        )
+    }
+
+    if (props.show){
+        return (
+            <Container style={styles.actionBandMultipleAction}>
+                <Button style={{width:'33.33%', height:'100%'}} transparent> 
+                    <Icon type='FontAwesome5' name='chevron-left' onPress={handleCloseDetails} style={{color:colours.dkGray}}/>
+                </Button>
+                
+                <Text style={styles.centerText}>Habits</Text>
+                <Text style={styles.placeholder} uppercase={false}>+</Text>
+            </Container>)
+    }
+    return null; 
 }
 export default TopBar;
 
